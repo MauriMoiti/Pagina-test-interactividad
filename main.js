77274edfc9b0234fc240aea1873ff2d77653348a
MauriMoiti <maurimoitii@gmail.com>
2023-06-21 21:00:23 -0300

    En el index.html: Cambios en el nombre de las etiquetas div del menu de login, menu de sign in, tambien  del div que anida a los buttons Sign in y Login pertenecientes al header. Se módifico el nombre de los botones del Nav, sustituyendo los nombres de element 1, 2,3,4,5 por  Comunidad, Reseñas, Noticias, Blogs, Soporte. Por otro lado se le agrego una class al titulo h2. En la etiqueta textarea se aumento el atributo maxlength pasando de 500 a 5000 caracteres máximos.En el archivo main.css a las nuevas clases del menu login "inactive_menuLogin" y del menu Sign in "inactive_menuSignIn se le agregos agrego por defecto el display none, donde por defecto con la alteración del DOM del JS, van a mutar a las clases "menu_login" y "menu_signIn". Luego al textarea se le asigno un tamaño por defecto de height 400px y width 470px. Por último se creo la versión de estilos responsive para pantallas de hasta máximo 640px.  En el archivo main.js: se modifico los nuevos nombres para las referencias de los querySelector de las constantes menuLogin y menuCreateAccount. En las function de buttonLogin como la de buttonSignIn se modifico el nombre de la clase a la que hace referencia menuCreateAccount.classList.toggle y menuLogin.classList.toggle, colocandoles como referencia sus nuevos nombres de clase.

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const buttonLogin = document.querySelector('.button_login');
-const menuLogin = document.querySelector('.menu_login');
+const menuLogin = document.querySelector('.inactive_menuLogin');
 const buttonSignIn = document.querySelector('.button_signIn')
-const menuCreateAccount = document.querySelector('.menu_signIn');
+const menuCreateAccount = document.querySelector('.inactive_menuSignIn');
+const textAreaPost = document.querySelector('.textarea_post');
 
 // cuando se ejecuta el evento del click, la function toggleMenuLogin, por default elimina la clase menu_login y agrega la clase inactive, teniendo esta última display none. Al volver a hacer click en el button, sucede a la inversa. Se elimina la clase inactive y se agrega la clase menu_login haciendo que el menu vuelva a aparecer con el css cargado a su respectiva clase.
 buttonLogin.addEventListener ('click', toggleMenuLogin);
 
 function toggleMenuLogin() {
-    menuLogin.classList.toggle('inactive');
+    menuLogin.classList.toggle('inactive_menuLogin');
     menuLogin.classList.toggle('menu_login');
 
      // Ocultar el menú de "Create account"
-     menuCreateAccount.classList.add('inactive');
+     menuCreateAccount.classList.add('inactive_menuSignIn');
      menuCreateAccount.classList.remove('menu_signIn');  
 };
 
@@ -19,10 +20,23 @@ function toggleMenuLogin() {
 buttonSignIn.addEventListener('click', toggleMenuCreateAccount)
 
 function toggleMenuCreateAccount() {
-    menuCreateAccount.classList.toggle('inactive');
+    menuCreateAccount.classList.toggle('inactive_menuSignIn');
     menuCreateAccount.classList.toggle('menu_signIn');
 
      // Ocultar el menú de "Login"
-     menuLogin.classList.add('inactive');
+     menuLogin.classList.add('inactive_menuLogin');
      menuLogin.classList.remove('menu_login');
 };
+
+// function para modificar textarea. Verifica si la pantalla tiene un ancho máximo de 640px
+
+// function checkScreenWidth () {
+//     if (window.innerWidth <= 640) {
+//         textAreaPost.setAttribute('cols', '30');
+//     } else {
+//         textAreaPost.setAttribute('cols', '60');
+//     }
+// }
+// // Verifica el ancho de la pantalla al cargar y al redimensionar
+// window.addEventListener('load', checkScreenWidth);
+// window.addEventListener('resize', checkScreenWidth);
